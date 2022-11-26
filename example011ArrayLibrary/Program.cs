dbd46a5935016a942e02893243e0f0dd95005984
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-26 23:55:36 +0700

    / add  example 011  First Occurrence

--- example011ArrayLibrary/Program.cs
+++ example011ArrayLibrary/Program.cs
@@ -18,6 +18,30 @@ void PrintArray(int[]  col)
         Console.WriteLine(col[position]);
         position ++;
     }
-    
+
 }
-int[] array = new int[10];
+
+int IndexOf(int[] collection, int find)
+{
+    int counte = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < counte)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index ++;
+    }
+        return position;
+}
+
+int[] array = new int[10];  
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
