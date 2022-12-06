7bcd614327380d25ad47288fce26bd736dc3d43b
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-06 22:31:22 +0700

    / add  commit example014

--- example014/Program.cs
+++ example014/Program.cs
@@ -14,20 +14,22 @@ void PrintArray(int[] array)// метод вывода элементов мас
 }
 PrintArray(arr); 
 
-void SelectionSort (int[] array) // метод сортировки(упорядочивания) элементов массива
+void SelectionSort(int[] array) // метод сортировки(упорядочивания) элементов массива
 { 
-    for (int i = 0; i < array.length - 1 ; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] < array[minPosition]) 
+            {
+                minPosition = j;
+            }    
         }
         int temporary = array[i];
         array[i] = array[minPosition];
         array[minPosition] = temporary;
     }
-
 }
 PrintArray(arr);
 SelectionSort(arr);
