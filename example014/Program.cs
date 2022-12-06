e5e330f8e9dc4fe5bb61f4fee6a8d63403da700d
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-06 22:07:15 +0700

    / add  commit example014

---
+++ example014/Program.cs
@@ -0,0 +1,35 @@
+﻿// example014 - программа упорядочивания данных массива - алгоритм сортировки методом
+// выбора или алгоритм сортировки методом макса("танец с номерами" - от 1 до 8)
+
+int[]arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)// метод вывода элементов массива
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr); 
+
+void SelectionSort (int[] array) // метод сортировки(упорядочивания) элементов массива
+{ 
+    for (int i = 0; i < array.length - 1 ; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
